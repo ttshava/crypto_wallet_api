29fa8959b4fc2d392e2ef6610ddf9adc9f6a6817
Timothy Shava <ttshava@gmail.com>
2022-01-07 08:35:00 -0800

    Update settings.py

--- wallet/settings.py
+++ wallet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-cm4$z7aqz+i-$utztq09gi5z^u)dysqv@)ah@zy81t+^vz=^oi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['zeply-api.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
